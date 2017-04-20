759c922cb8d46f1c04728c99e47184e87fab8692
Mark Greenwald <markerdink@gmail.com>
2017-04-19 20:58:57 -0700

    Ioc, ViewModels, Tests

---
+++ Source/ProjectOno/ProjectOno.Core/Environment/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectOno.Core.Environment
+{
+    public interface IViewModel : IObservable
+    {
+        IViewModel Parent { get; }
+    }
+
+    public abstract class ViewModel : Observable, IViewModel
+    {
+        private IIocContainer _container;
+        private IViewModel _parent;
+        IViewModel IViewModel.Parent { get { return _parent; } }
+
+        protected abstract void OnReady();
+
+        public TViewModel CreateChild<TViewModel>()
+            where TViewModel : ViewModel
+        {
+            return Factory.Create<TViewModel>(_container, this);
+        }
+
+        public static class Factory
+        {
+            public static TViewModel Create<TViewModel>(IIocContainer container, IViewModel parent)
+                where TViewModel : ViewModel
+            {
+                var viewmodel = (TViewModel)container.Resolve(typeof(TViewModel));
+                viewmodel._parent = parent;
+                var scope = container.CreateScope();
+                scope.AddInstance(typeof(IViewModel), viewmodel);
+                viewmodel._container = scope;
+                viewmodel.OnReady();
+                return viewmodel;
+            }
+        }
+    }
+}
